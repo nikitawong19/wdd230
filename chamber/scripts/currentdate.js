a52ceec252c9cd80be4e01632c3cb7a56722820c
nikitawong19 <iamspecial19@gmail.com>
2022-10-20 15:36:55 +0800

    Add JS for event reminder.

--- chamber/scripts/currentdate.js
+++ chamber/scripts/currentdate.js
@@ -1,4 +1,14 @@
 // A shortcut method for creating dynamic date that will automatically update the date every day.
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+document.querySelector('.date').textContent = new Date().toLocaleDateString('en-US', options);
+
+// script for event reminder
+let date = new Date();
+let weekday = date.toLocaleDateString("default",{weekday:"long"});
+if (weekday === "Monday" || weekday === "Tuesday") {
+    document.querySelector(".reminder").textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+} else {
+    let reminder = document.querySelector(".reminder");
+    reminder.remove();
+}
 
